04d10988074c491717a43b16c92f759156bf3d25
Nicholas Junge <nicholas.junge@web.de>
2021-01-24 16:11:08 +0100

    Typescript lib

--- lib/api.ts
+++ lib/api.ts
@@ -4,19 +4,23 @@ import matter from "gray-matter";
 
 const CONTENT_ROOT = "content";
 
-export function getPostSlugs(sectionType) {
+export function getPostSlugs(sectionType: string) {
   const postsDirectory = join(process.cwd(), CONTENT_ROOT, sectionType);
   return fs.readdirSync(postsDirectory);
 }
 
-export function getPostBySlug(slug, sectionType, fields = []) {
+export function getPostBySlug(slug: string, sectionType: string, fields: string[] = []) {
   const realSlug = slug.replace(/\.md$/, "");
   const postsDirectory = join(process.cwd(), CONTENT_ROOT, sectionType);
   const fullPath = join(postsDirectory, `${realSlug}.md`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
   const { data, content } = matter(fileContents);
 
-  const items = {};
+  type Items = {
+    [key: string]: string
+  }
+
+  const items: Items = {};
 
   // Ensure only the minimal needed data is exposed
   fields.forEach((field) => {
@@ -37,7 +41,7 @@ export function getPostBySlug(slug, sectionType, fields = []) {
   return items;
 }
 
-export function getAllPosts(sectionType, fields = []) {
+export function getAllPosts(sectionType:string, fields: string[] = []) {
   const slugs = getPostSlugs(sectionType);
   const posts = slugs
     .map((slug) => getPostBySlug(slug, sectionType, fields))
