c6c04054de61a2a7ece02b1e6aac68b9671cdb1b
Nicholas Junge <nicholas.junge@web.de>
2021-01-24 17:55:04 +0100

    Intro component

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,8 +1,12 @@
-export default function Intro() {
+type Props = {
+  children: string
+}
+
+export default function Intro({ children }: Props) {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Latest.
+        {children}
       </h1>
     </section>
   );
