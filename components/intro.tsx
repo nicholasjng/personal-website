a0c67cb07fc6ee771f30107badec9f61cfaa0b1e
Nicholas Junge <nicholas.junge@web.de>
2021-01-24 21:04:49 +0100

    Smaller intro font

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,7 +5,7 @@ type Props = {
 export default function Intro({ children }: Props) {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+      <h1 className="text-5xl md:text-7xl font-bold tracking-tighter leading-tight md:pr-8">
         {children}
       </h1>
     </section>
