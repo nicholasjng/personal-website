baa4f5540ba1d3fdf742ed5296e95a9e53bbb396
Nicholas Junge <nicholas.junge@web.de>
2021-02-01 21:56:41 +0100

    Change to remote URL of hosted website

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,2 @@
-export const HOME_OG_IMAGE = "/assets/og/og-image.png";
+export const HOME_OG_IMAGE = "https://www.nicholasjunge.com/assets/og/og-image.png";
 export const GITHUB_REPO = "https://github.com/njunge94/personal-website";
