a5d11e8bd41380754e068c2ad1f485adfe0c6d83
Nicholas Junge <nicholas.junge@web.de>
2021-02-03 13:52:35 +0100

    Fix social footer bug by prepending base production URL

--- components/social-footer.tsx
+++ components/social-footer.tsx
@@ -12,27 +12,30 @@ import {
 } from 'react-share';
 
 import { Router, useRouter } from 'next/router';
+import { BASE_URL } from '../lib/constants';
 
 export default function SocialFooter() {
   const router = useRouter();
   const location = router.asPath;
 
+  const url = BASE_URL + (location === "/" ? "" : location)
+
   return (
     <nav className="space-x-2 sm:self-center md:self-start">
       <h3 className="pl-2 text-2xl font-bold my-2">Share this page:</h3>
-      <EmailShareButton url={location}>
+      <EmailShareButton url={url}>
         <EmailIcon size={32} round />
       </EmailShareButton>
-      <FacebookShareButton url={location}>
+      <FacebookShareButton url={url}>
         <FacebookIcon size={32} round />
       </FacebookShareButton>
-      <LinkedinShareButton url={location}>
+      <LinkedinShareButton url={url}>
         <LinkedinIcon size={32} round />
       </LinkedinShareButton>
-      <RedditShareButton url={location}>
+      <RedditShareButton url={url}>
         <RedditIcon size={32} round />
       </RedditShareButton>
-      <TwitterShareButton url={location}>
+      <TwitterShareButton url={url}>
         <TwitterIcon size={32} round />
       </TwitterShareButton>
     </nav>
