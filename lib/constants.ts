63b2e68e5c287e04f80fdf4e9c57eb89c6b32ac2
Nicholas Junge <nicholas.junge@web.de>
2021-02-03 13:54:29 +0100

    Make base URL a constant for use in various scenarios

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,3 @@
 export const HOME_OG_IMAGE = "https://www.nicholasjunge.com/assets/og/og-image.png";
 export const GITHUB_REPO = "https://github.com/njunge94/personal-website";
+export const BASE_URL = "https://www.nicholasjunge.com"
