e0246b1abd255fb9043bc3b70bff8b4c46f10678
Nicholas Junge <nicholas.junge@web.de>
2021-02-05 23:27:56 +0100

    Change OG Image to jpg to save 600KB

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
-export const HOME_OG_IMAGE = "https://www.nicholasjunge.com/assets/og/og-image.png";
+export const HOME_OG_IMAGE = "https://www.nicholasjunge.com/assets/og/og-image.jpg";
 export const GITHUB_REPO = "https://github.com/njunge94/personal-website";
 export const BASE_URL = "https://www.nicholasjunge.com"
 export const imageResizeTargets = [384, 512, 768, 1024, 1280, 1536, 2048];
