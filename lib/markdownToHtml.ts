753a1a8472007bb0a5e4ebb85cea867823cd28f5
Nicholas Junge <nicholas.junge@web.de>
2021-02-18 15:29:45 +0100

    Move to more granular remark stack, add math

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,8 +1,12 @@
-import remark from "remark";
-import html from "remark-html";
+import unified from "unified";
+import parse from "remark-parse";
+import remark2rehype from "remark-rehype";
+import katex from "rehype-katex";
+import stringify from "rehype-stringify";
 import math from "remark-math";
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(math).use(html).process(markdown);
-  return result.toString();
+  const result = await unified().use(parse).use(math).use(remark2rehype).use(katex).use(stringify).process(markdown);
+
+  return result.contents;
 }
