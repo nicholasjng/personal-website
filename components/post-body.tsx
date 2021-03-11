7d774c934b18c3d8005ce73e2c9ec56c16b2e8d8
Nicholas Junge <nicholas.junge@web.de>
2021-03-11 20:51:03 +0100

    Add tailwind typography to markdown class

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -9,7 +9,7 @@ export default function PostBody({ content }: Props) {
   return (
     <div className="max-w-2xl mx-auto">
       <div
-        className={markdownStyles.markdown}
+        className="prose prose-blue lg:prose-xl"
         // IMPORTANT: DOMPurify sanitizing on server-side
         // eslint-disable-next-line react/no-danger
         dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(content) }}
