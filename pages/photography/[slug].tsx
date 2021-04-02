de82f4b0e51091ed28e1b2ce64c8a435a424a8a5
Nicholas Junge <nicholas.junge@web.de>
2021-04-02 16:26:41 +0200

    Add photography page slug

---
+++ pages/photography/[slug].tsx
@@ -0,0 +1,95 @@
+import Head from "next/head";
+import { Router, useRouter } from "next/router";
+import ErrorPage from "next/error";
+import PostBody from "../../components/post-body";
+import PostHeader from "../../components/post-header";
+import Layout from "../../components/layout";
+import { getPostBySlug, getAllPosts } from "../../lib/api";
+import PostTitle from "../../components/post-title";
+import markdownToHtml from "../../lib/markdownToHtml";
+import PostType from "../../types/post";
+
+const section = "photography";
+
+type Props = {
+  post: PostType
+  morePosts: PostType[]
+  preview: boolean
+}
+
+export default function Post({ post, morePosts, preview }: Props) {
+  const router = useRouter();
+  if (!router.isFallback && !post?.slug) {
+    return <ErrorPage statusCode={404} />;
+  }
+
+  return (
+    <Layout preview={preview}>
+      {router.isFallback ? (
+        <PostTitle>Loading…</PostTitle>
+      ) : (
+        <>
+          <article className=" mb-32">
+            <Head>
+              <title>
+                {post.title} | Nicholas Junge
+              </title>
+              <meta property="og:image" content={post.ogImage.url} />
+            </Head>
+            <PostHeader
+              title={post.title}
+              coverImage={post.coverImage}
+              date={post.date}
+              section={post.section}
+              author={post.author}
+            />
+            <PostBody content={post.content} />
+          </article>
+        </>
+      )}
+    </Layout>
+  );
+}
+
+type Params = {
+  params: {
+    slug: string,
+  }
+}
+
+export async function getStaticProps({ params }: Params) {
+
+  const post = getPostBySlug(params.slug, section, [
+    "title",
+    "date",
+    "slug",
+    "section",
+    "author",
+    "content",
+    "ogImage",
+    "coverImage",
+  ]);
+  const content = await markdownToHtml(post.content || "");
+
+  return {
+    props: {
+      post: {
+        ...post,
+        content,
+      },
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const posts = getAllPosts(section, ["slug"]);
+
+  return {
+    paths: posts.map((post) => ({
+      params: {
+        slug: post.slug,
+      },
+    })),
+    fallback: false,
+  };
+}
