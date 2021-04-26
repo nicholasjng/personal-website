18b87d902922989bac20827da2d8700d607f688f
Nicholas Junge <nicholas.junge@web.de>
2021-04-26 12:01:03 +0200

    Make constant attributes for getStaticProps

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,6 @@
 export const HOME_OG_IMAGE = "https://www.nicholasjunge.com/assets/og/og-image.jpg";
 export const GITHUB_REPO = "https://github.com/njunge94/personal-website";
-export const BASE_URL = "https://www.nicholasjunge.com"
+export const BASE_URL = "https://www.nicholasjunge.com";
 export const imageResizeTargets = [384, 512, 768, 1024];
+export const DEFAULT_ATTRIBUTES = ["title", "date", "slug", "section", "author", "ogImage", "excerpt"];
+export const CONTENT_ATTRIBUTE = ["content"];
