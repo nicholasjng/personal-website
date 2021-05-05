2a7e53d99cd2ab6b42deac339481dcdd590a3be6
Nicholas Junge <nicholas.junge@web.de>
2021-05-05 17:41:31 +0200

    Increase post header margin

--- components/post-title.tsx
+++ components/post-title.tsx
@@ -3,7 +3,7 @@ type Props = {
 };
 
 const PostTitle = ({ children }: Props) => (
-  <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
+  <h1 className="pt-20 text-4xl md:text-5xl lg:text-6xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
     {children}
   </h1>
 );
