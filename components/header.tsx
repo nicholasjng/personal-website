566fda3f7fe4d0d528f10142f61cfbb840d1fe42
Nicholas Junge <nicholas.junge@web.de>
2022-05-30 09:37:11 +0200

    Fix header component

--- components/header.tsx
+++ components/header.tsx
@@ -51,31 +51,31 @@ const HeaderLink = ({ isActive, title, to, spanColor }: LinkProps) => (
   </Link>
 );
 
-const Header = ({ location }: Props) => (
-  <header
-    className="sticky top-0 z-10 mb-5 bg-white border-b-2 border-black 
+export default function Header({ location }: Props) {
+  return (
+    <header
+      className="sticky top-0 z-10 mb-5 bg-white border-b-2 border-black 
   whitespace-nowrap py-4 overflow-x-scroll xl:overflow-x-auto"
-  >
-    <h1
-      className="text-2xl md:text-4xl font-bold tracking-tight 
-    md:tracking-tighter inline-block ml-5 md:ml-10 mr-5 md:mr-0"
     >
-      <Link href="/">
-        <a className="hover:underline">nj.</a>
-      </Link>
-    </h1>
-    <nav className="inline-block absolute bottom-1 sm:right-1 md:right-12">
-      {navHeader.items.map((link: LinkItem) => (
-        <HeaderLink
-          key={link.title}
-          isActive={location.includes(link.activeSelector)}
-          title={link.title}
-          to={link.to}
-          spanColor={link.spanColor}
-        />
-      ))}
-    </nav>
-  </header>
-);
-
-export default Header;
+      <h1
+        className="text-2xl md:text-4xl font-bold tracking-tight 
+    md:tracking-tighter inline-block ml-5 md:ml-10 mr-5 md:mr-0"
+      >
+        <Link href="/">
+          <a className="hover:underline">nj.</a>
+        </Link>
+      </h1>
+      <nav className="inline-block absolute bottom-1 sm:right-1 md:right-12">
+        {navHeader.items.map((link: LinkItem) => (
+          <HeaderLink
+            key={link.title}
+            isActive={location.includes(link.activeSelector)}
+            title={link.title}
+            to={link.to}
+            spanColor={link.spanColor}
+          />
+        ))}
+      </nav>
+    </header>
+  );
+}
